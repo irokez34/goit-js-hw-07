81c8c97814cb47a11b6e6f7ea46f6c477154dd1b
irokez34 <jhmdaurel@gmail.com>
2023-10-02 20:09:49 +0300

    task_2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const galleryEl = document.querySelector('.gallery')
+
+const makrup = galleryItems.map(({preview, original, description}) => {
+  return`<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+     <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`}).join('');
+  galleryEl.insertAdjacentHTML('afterbegin',makrup);
+  
+  
+  galleryEl.addEventListener('click', modalOpen)
+
+  function modalOpen (evt){
+    const target = evt.target;
+    evt.preventDefault();
+    if (target === evt.currentTarget) {
+      return;
+    }
+     const gallery = new SimpleLightbox('.gallery a',{
+      captionsData: 'alt', 
+      captionDelay: 250,
+      close: false,
+      enableKeyboard: true,
+      captionPosition: 'bottom',
+      captionSelector: 'img',
+      overlayOpacity: 0.2,
+    })
+console.log(gallery)
+}
+
+
+
+
+
+
+
+
+
+
 console.log(galleryItems);
